99fbe874992a67e9dc48b7165aae5a9d695bbb79
Chester.N.Lim <chester.n.lim@gmail.com>
2012-06-29 22:26:09 +0000

    Added USB Readout standalone project

---
+++ USB_Readout/software/README.txt
@@ -0,0 +1,4 @@
+This folder contains C/C++ programs written in Linux and Windows. 
+
+Linux: test_usb & test2
+Windows: test_usb.exe
---
+++ USB_Readout/software/windows/test_usb.h
@@ -0,0 +1,6 @@
+#include <cstdlib>
+#include <Windows.h>
+#include <BaseTsd.h>
+#include <WinDef.h>
+#include <WinNT.h>
+#include "CyAPI.h"
