1c1c7d3c5999b3d6ac026aa36e25d50229344192
Kurtis N. <psistarpsi@users.noreply.github.com>
2012-11-05 23:41:10 +0000

    Minor fixes to software after testing on another system.

--- USB_and_fiber_readout/software/linux/src/SCROD_read_temp.cpp
+++ USB_and_fiber_readout/software/linux/src/SCROD_read_temp.cpp
@@ -43,6 +43,7 @@ int main(){
 	                                      0x0295,  //   send addr + read
 	                                      0x0400,  //6. read w/ no ack
 	                                      0x1000}; //7. stop
+	//Send the I2C commands
 	for (int i = 0; i < N_commands; ++i) {
 		command_stack.ClearPacket();
 		command_stack.CreateCommandPacket(command_id,board_id);
@@ -79,7 +80,40 @@ int main(){
 //		sleep(1);
 //		getchar();
 	}
+	//Read back what's on the I2C bus after the last read command
+	for (int i = 0; i < 1; ++i) {
+		command_stack.ClearPacket();
+		command_stack.CreateCommandPacket(command_id,board_id);
+		command_id++;
+		command_stack.AddReadToPacket(read_reg);
+		command_stack.PrintPacket();
 
+		outbuf = command_stack.AssemblePacket(size);
+		byte_reverse(outbuf,command_stack.GetTotalSize());
+
+	        usb_XferData((OUT_ADDR | LIBUSB_ENDPOINT_OUT), (unsigned char *) outbuf, size*4, TM_OUT);
+		delete[] outbuf;
+
+		//RX
+	 	int retval = usb_XferData((IN_ADDR | LIBUSB_ENDPOINT_IN), p_inbuf, 512*sizeof(unsigned int), TM_OUT);
+		cout << "Read out " << retval << " bytes." << endl;
+		while (retval > 0) {
+			byte_reverse(inbuf,retval/4);
+			for(j=0;j<retval/4;j++) {
+				printf("inbuf[%d]=0x%.8X\t",j,inbuf[j]);
+				for (int k = 3; k >= 0; --k) {
+					unsigned int mask = 0x000000FF;
+					mask = mask << (k*8);
+					mask = mask & inbuf[j];
+					mask = mask >> (k*8);
+					char this_char= (char) mask;
+					printf("%c",this_char);
+				}
+				printf("\n");
+			}
+	       		retval = usb_XferData((IN_ADDR | LIBUSB_ENDPOINT_IN), p_inbuf, 512*sizeof(unsigned int), TM_OUT);
+		}
+	}
         close_usb();
 }
 
