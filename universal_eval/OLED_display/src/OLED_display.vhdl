e167974a367ec363d0dc019fa4043bc8d2609268
Matt Andrew <mzandrew@users.noreply.github.com>
2014-05-06 19:40:40 +0000

    interesting-fixed-pattern

--- universal_eval/OLED_display/src/OLED_display.vhdl
+++ universal_eval/OLED_display/src/OLED_display.vhdl
@@ -26,7 +26,7 @@ entity my_module_name is
 		clock_40                : in    STD_LOGIC;
 		sync                    :   out STD_LOGIC;
 		data_bus                : inout STD_LOGIC_VECTOR (7 downto 0);
-		enable_active_low       :   out STD_LOGIC;
+		enable       :   out STD_LOGIC;
 		read_write_not          :   out STD_LOGIC;
 		chip_select_active_low  :   out STD_LOGIC;
 		data_command_not        :   out STD_LOGIC;
@@ -41,7 +41,7 @@ entity my_module_name is
 	attribute loc of clock_150              : signal is "p181";
 	attribute loc of clock_40               : signal is "p79";
 	attribute loc of data_bus               : signal is "p178,p176,p175,p172,p171,p169,p168,p166";
-	attribute loc of enable_active_low      : signal is "p165";
+	attribute loc of enable      : signal is "p165";
 	attribute loc of read_write_not         : signal is "p162";
 	attribute loc of chip_select_active_low : signal is "p161";
 	attribute loc of data_command_not       : signal is "p156";
@@ -61,7 +61,7 @@ architecture my_module_name_architecture of my_module_name is
 	signal internal_clock_20  : std_logic := '0';
 	signal internal_clock_3   : std_logic := '0';
 	signal internal_data_bus               : std_logic_vector(7 downto 0) := x"00";
-	signal internal_enable_active_low      : std_logic := '1';
+	signal internal_enable      : std_logic := '1';
 	signal internal_read_write_not         : std_logic := '1';
 	signal internal_chip_select_active_low : std_logic := '1';
 	signal internal_data_command_not       : std_logic := '1';
@@ -72,12 +72,21 @@ architecture my_module_name_architecture of my_module_name is
 	constant size_of_reset_counter : integer := 16;
 	signal reset_counter : unsigned(size_of_reset_counter-1 downto 0) := (others => '0');
 	signal initialization_counter : unsigned(11 downto 0) := (others => '0');
+	signal initialization_phase : STD_LOGIC := '0';
+	signal normal_counter : unsigned(11 downto 0) := (others => '0');
+	signal individual_transaction_counter : unsigned(2 downto 0) := (others => '0');
+	signal x : unsigned(7 downto 0) := (others => '0');
+	signal y : unsigned(5 downto 0) := (others => '0');
+	--procedure chip_select_active is
+	--begin
+	--	internal_chip_select_active_low <= '0';
+	--end chip_select_active;
 begin
 	--
 	internal_clock_150 <= clock_150;
 	internal_clock_40 <= clock_40;
 	sync <= internal_sync;
-	enable_active_low <= internal_enable_active_low;
+	enable <= internal_enable;
 	read_write_not <= internal_read_write_not;
 	chip_select_active_low <= internal_chip_select_active_low;
 	data_command_not <= internal_data_command_not;
@@ -95,7 +104,7 @@ begin
 		);
 	mapper_3MHz : entity work.clock_enable_generator
 		generic map (
-			DIVIDE_RATIO => 50
+			DIVIDE_RATIO => 45
 		)
 		port map (
 			CLOCK_IN         => internal_clock_150,
@@ -106,63 +115,135 @@ begin
 	begin
 		if rising_edge(internal_clock_150) then
 			if (clock_enable_1kHz = '1') then
-				if (reset_counter < 1000) then
+				if (reset_counter < 100) then
 					internal_reset_active_low <= '0';
 					reset_counter <= reset_counter + 1;
 					internal_data_bus               <= x"00";
-					internal_enable_active_low      <= '1';
+					internal_enable      <= '1';
 					internal_read_write_not         <= '1';
-					internal_chip_select_active_low <= '1';
+					internal_chip_select_active_low <= '0';
 					internal_data_command_not       <= '1';
 					internal_reset_active_low       <= '0';
-					internal_interface_type_select  <= "11";
+					internal_interface_type_select  <= "11"; -- 6800 mode
+					initialization_phase <= '1';
+					initialization_counter <= x"000";
+					normal_counter <= x"000";
 				else
 					internal_reset_active_low <= '1';
 				end if;
 			end if;
 			if (clock_enable_3MHz = '1') then
-				internal_sync <= not internal_sync;
-				if (internal_reset_active_low = '1') then
+				if (internal_reset_active_low = '1' and initialization_phase = '1') then
 				--	if (initialization_counter < 1000) then
 						initialization_counter <= initialization_counter + 1;
+				--	else
+				--		initialization_phase <= '0';
 				--	end if;
 					if (initialization_counter < 4) then
-						internal_read_write_not <= '0';
+						internal_read_write_not   <= '0';
 						internal_data_command_not <= '0';
-						internal_data_bus <= x"00";
+						--internal_data_bus         <= x"00";
 					--
 					elsif (initialization_counter < 5) then
-						internal_data_bus <= x"a5";
+						--internal_data_bus <= x"a5"; -- all pixels on
+						internal_data_bus <= x"a6"; -- normal mode
 					elsif (initialization_counter < 6) then
-						internal_enable_active_low <= '1';
+						internal_enable <= '1';
 					elsif (initialization_counter < 7) then
 						internal_chip_select_active_low <= '0';
-					elsif (initialization_counter < 8) then
 					elsif (initialization_counter < 9) then
 						internal_chip_select_active_low <= '1';
-						internal_enable_active_low <= '0';
+						internal_enable <= '0';
 					--
 					elsif (initialization_counter < 10) then
-						internal_data_bus <= x"af";
+						internal_data_bus <= x"af"; -- display on
 					elsif (initialization_counter < 11) then
-						internal_enable_active_low <= '1';
+						internal_enable <= '1';
 					elsif (initialization_counter < 12) then
 						internal_chip_select_active_low <= '0';
-					elsif (initialization_counter < 13) then
 					elsif (initialization_counter < 14) then
 						internal_chip_select_active_low <= '1';
-						internal_enable_active_low <= '0';
+						internal_enable <= '0';
 					--
 					elsif (initialization_counter < 15) then
-						internal_data_bus <= x"00";
-						internal_enable_active_low      <= '0';
-						internal_read_write_not         <= '1';
+						--internal_data_bus <= x"00"; -- enable grayscale mode with custom lut
+						internal_data_bus <= x"b9"; -- use linear grayscale lut
+					elsif (initialization_counter < 16) then
+						internal_enable <= '1';
+					elsif (initialization_counter < 17) then
+						internal_chip_select_active_low <= '0';
+					elsif (initialization_counter < 18) then
 						internal_chip_select_active_low <= '1';
+						internal_enable <= '0';
+					--
+					elsif (initialization_counter < 19) then
+						internal_data_bus <= x"5c"; -- write to display ram
+					elsif (initialization_counter < 20) then
+						internal_enable <= '1';
+					elsif (initialization_counter < 21) then
+						internal_chip_select_active_low <= '0';
+					elsif (initialization_counter < 22) then
+						internal_chip_select_active_low <= '1';
+						internal_enable <= '0';
+					--
+					elsif (initialization_counter < 23) then
+						--internal_data_bus <= x"00";
+						--internal_enable      <= '0';
+						--internal_read_write_not         <= '1';
+						--internal_chip_select_active_low <= '1';
+						--internal_data_command_not       <= '1';
 						internal_data_command_not       <= '1';
+						normal_counter <= x"000";
+						individual_transaction_counter <= "000";
+						x <= "00000000";
+						y <=   "000000";
+--						internal_data_bus <= x"12";
+					elsif (initialization_counter < 24) then
+						initialization_phase <= '0';
+--						internal_data_bus <= x"55";
 					end if;
+				end if;
+--			end if;
+--			if (clock_enable_3MHz = '1') then
+				if (initialization_phase = '0') then
+					if (individual_transaction_counter < 6) then
+						individual_transaction_counter <= individual_transaction_counter + 2;
+--						internal_data_bus <= x"99";
+					else
+						normal_counter <= normal_counter + 1;
+						individual_transaction_counter <= "000";
+--						internal_data_bus <= x"ab";
+					end if;
+					if (individual_transaction_counter < 1) then
+						internal_sync <= not internal_sync;
+--						internal_data_bus <= x"ee";
+					elsif (individual_transaction_counter < 2) then
+						internal_sync <= not internal_sync;
+						--internal_data_bus <= std_logic_vector(normal_counter(3 downto 0))
+						internal_data_bus <= "0000"
+						                   & std_logic_vector(normal_counter(9 downto 8)) & "00";
+						                   --& std_logic_vector(normal_counter(2 downto 0)) & '0';
+						--internal_data_bus <= x"47";
+					elsif (individual_transaction_counter < 3) then
+						internal_enable <= '1';
+					elsif (individual_transaction_counter < 4) then
+						internal_chip_select_active_low <= '0';
+					elsif (individual_transaction_counter < 5) then
+						internal_chip_select_active_low <= '1';
+						internal_enable <= '0';
+					end if;
+					--
 				end if;
 			end if;
 		end if;
 	end process;
 end my_module_name_architecture;
+
+--use work.OLED_control.all;
+--package OLED_control is
+--	procedure chip_select_active();
+--end OLED_control;
+
+--package body OLED_control is
+--end OLED_control;
 
